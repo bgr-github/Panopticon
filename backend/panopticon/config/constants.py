785233fd09029891726200936a8655d7a1afe92c
bgr-github <beagerr@gmail.com>
2026-09-01 13:36:18 +0100

    Ingestion DB connection

---
+++ backend/panopticon/config/constants.py
@@ -0,0 +1,9 @@
+# Common fields shared between all events
+COMMON_FIELDS: set = {
+    "id",
+    "session_id",
+    "event_type",
+    "src_ip",
+    "src_port",
+    "timestamp",
+}
--- backend/panopticon/ingestion/worker.py
+++ backend/panopticon/ingestion/worker.py
@@ -1,8 +1,10 @@
 import asyncio
-import time
+import psycopg
+import json
 from typing import cast
 from redis.asyncio import Redis
 from panopticon.config.settings import settings
+from panopticon.config.constants import COMMON_FIELDS
 from panopticon.observability.logging import logger
 
 # please ignore.
@@ -17,10 +19,14 @@ class IngestionWorker:
     last_id: str
 
     def __init__(self) -> None:
-        self.redis = Redis.from_url(str(settings.redis.dsn), db=0, decode_responses=True)
-        self.last_id = "0"
-
-        # object pushes to both postgresql and server-side events
+        self.redis = Redis.from_url(
+            str(settings.redis.dsn),
+            db=0,
+            decode_responses=True,
+            socket_timeout=None,
+        )
+        self.db = psycopg.connect(settings.database.dsn)
+        self.last_id = "$"
 
     async def read_batch(self) -> list[str]:
         """Read events from the Redis stream"""
@@ -30,13 +36,14 @@ class IngestionWorker:
             RedisStreamBatch,
             await self.redis.xread(
                 streams={settings.redis.stream_name: self.last_id},
-                count=10,  # TODO: Blocks of 10 events, change to 100 during prod
-                block=1000,  # TODO: Every 1 second, change to 5 during prod
+                count=10,  # 10 events
+                block=5000,  # check every 5 seconds
             ),
         )
 
         events: list[str] = []
 
+        # Adds raw event json to event list
         for _, messages in event_batch:
             for message_id, fields in messages:
                 self.last_id = message_id
@@ -44,17 +51,45 @@ class IngestionWorker:
 
         return events
 
+    def store_db(self, event: str) -> None:
+        sql: str = (
+            """INSERT INTO events (event_id, session_id, event_type, src_ip, src_port, timestamp, payload) VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+        )
+
+        current_event: dict = json.loads(event)
+        payload: dict = {key: value for key, value in current_event.items() if key not in COMMON_FIELDS}
+
+        with self.db.cursor() as cursor:
+            cursor.execute(
+                sql,
+                (
+                    current_event["id"],
+                    current_event["session_id"],
+                    current_event["event_type"],
+                    current_event["src_ip"],
+                    current_event["src_port"],
+                    current_event["timestamp"],
+                    json.dumps(payload),
+                ),
+            )
+
+        self.db.commit()
+
 
 async def main() -> None:
     worker: IngestionWorker = IngestionWorker()
     logger.info("Ingestion", "Ingestion worker started.")
 
+    # Main ingestion loop
     while True:
         try:
             events = await worker.read_batch()
 
             if not events:
                 continue
+
+            for event in events:
+                worker.store_db(event)
 
             logger.debug("Ingestion", f"Read {len(events)} from redis")
         except Exception as e:
