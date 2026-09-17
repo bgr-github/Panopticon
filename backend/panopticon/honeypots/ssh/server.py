b4338ad619890b5ac448196a9200800d9c55b34d
bgr-github <george@beager.co.uk>
2026-09-17 10:10:52 +0100

    Added docstrings

--- backend/panopticon/honeypots/ssh/server.py
+++ backend/panopticon/honeypots/ssh/server.py
@@ -1,6 +1,7 @@
 import asyncio
 from asyncssh import SSHServer, SSHServerConnection
 from typing import Optional
+from panopticon.honeypots.ssh.shell import ShellSession
 
 
 class HoneypotServer(SSHServer):
@@ -9,22 +10,63 @@ class HoneypotServer(SSHServer):
         pass
 
     def connection_made(self, conn: SSHServerConnection) -> None:
+        """Callback made when a client tries to initially connect to the server.
+
+        Args:
+            conn (SSHServerConnection): User's connection object
+        """
+
         pass
 
     def connection_lost(self, exc: Optional[Exception]) -> None:
+        """Callback made when the client connection drops.
+
+        Args:
+            exc (Optional[Exception]): None if connection closes cleanly.
+        """
         pass
 
     def begin_auth(self, username: str) -> bool:
+        """Callback made when the client begins the authorisation process.
+
+        Args:
+            username (str): Username the client is trying to connect to the server with.
+
+        Returns:
+            bool: Whether the client is successful or not.
+        """
         return True
 
     def password_auth_supported(self) -> bool:
+        """Callback function to decide whether the client must authenticate or not.
+
+        Returns:
+            bool: Whether the client can proceed to authentication.
+        """
+
         return True
 
     def validate_password(self, username: str, password: str) -> bool:
+        """Callback made to validate the chosen password by the client, if they are allowed to authenticate.
+
+        Args:
+            username (str): Client username
+            password (str): Client password
+
+        Returns:
+            bool: Whether the client is validated or not.
+        """
+
         return True
 
-    def session_requested(self) -> bool:
-        return True
+    def session_requested(self) -> ShellSession:
+        """Callback made when the client is authenticate and a shell is requested.
+
+        Returns:
+            ShellSession: Client's session as an object.
+        """
+
+        return ShellSession()
 
 
 async def main() -> None:
